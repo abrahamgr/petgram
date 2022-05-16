9aa462a4156a88680c52624234c3312c0f6207e5
Abraham Galindo <abraham.garo@gmail.com>
2022-05-16 16:22:58 -0500

    Refactor styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { GlobalStyle } from "./GlobalStyles";
+import { GlobalStyle } from "./styles/GlobalStyles";
 import { ListOfCategories } from './components/list-of-categories'
 import { ListOfPhotocards } from "./components/list-of-photocards";
 import { Logo } from "./components/logo";
